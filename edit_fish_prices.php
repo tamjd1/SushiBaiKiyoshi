21ebbbc098f1275cb4a430e113560a9069980bdc
tomdavison12 <tomdavison12@gmail.com>
2014-03-20 20:05:55 -0400

    More admin Pages

---
+++ edit_fish_prices.php
@@ -0,0 +1,23 @@
+<?php
+$file = "edit_fish_prices.php";
+$title = "Sushi Bai Kiyoshi - Edit Fish Prices";
+$banner = "Sushi Bai Kiyoshi - Edit Fish Prices";
+$description = "This page is where the administrator is able to add fish price information";
+$date = "20/03/2014";
+
+require 'header.php';
+
+
+
+?>
+
+
+
+        <section id="MainContent">            
+         
+            
+            
+            
+        </section>
+            
+<?php include 'footer.php'; ?>
---
+++ edit_menu_items.php
@@ -0,0 +1,23 @@
+<?php
+$file = "edit_menu_items.php";
+$title = "Sushi Bai Kiyoshi - Edit Menu Items";
+$banner = "Sushi Bai Kiyoshi - Edit Menu Items";
+$description = "This page is where the administrator will be able to add/delete items currently on the menu";
+$date = "20/03/2014";
+
+require 'header.php';
+
+
+
+?>
+
+
+
+        <section id="MainContent">            
+         
+            
+            
+            
+        </section>
+            
+<?php include 'footer.php'; ?>
